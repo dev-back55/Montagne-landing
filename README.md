5dfb8c0066dbae65937649f6c8def4abc49ea7b8
Horacio Abitu <ahabitu@gmail.com>
2024-05-05 19:37:56 -0300

    first commit landing Montagne Company

---
+++ README.md
@@ -0,0 +1,16 @@
+<div align="center">
+  <div>
+    <img src="https://img.shields.io/badge/-Tailwind_CSS-black?style=for-the-badge&logoColor=white&logo=tailwindcss&color=06B6D4" alt="tailwindcss" />
+  </div>
+
+  <h3 align="center">TailwindCSS Practica - Montagne web site</h3>
+
+   <div align="center">
+     Esta landing page es una practica de TailwindCSS. Te invito a mi porfolio para que puedas ver mis otros desarrollos, tanto de practica como en producción. <a href="https://horacioabitu.com.ar/" target="_blank"><b>Horacio Abitú</b></a>
+     <br/>
+     <a href="https://twitter.com/AbituHoracio" target="_blank"><b>Mi twitter</b></a>
+     <br/>
+      Que disfrutes este repo, gracias!
+    </div>
+</div>
+
---
+++ src/assets/images/index.js
@@ -0,0 +1,46 @@
+import bigShoe1 from './big-shoe1.png'
+import bigShoe2 from './big-shoe2.png'
+import bigShoe3 from './big-shoe3.png'
+
+import customer3 from './customer3.png'
+import customer4 from './customer4.png'
+
+import footerLogo from './footer-logo.svg'
+import montagneLogo from './montagne-logo.svg'
+
+import offer from './offer.png'
+
+import shoe4 from './shoe4.png'
+import shoe5 from './shoe5.png'
+import shoe6 from './shoe6.png'
+import shoe7 from './shoe7.png'
+import shoe8 from './shoe8.png'
+
+import thumbnailBackground from './thumbnail-background.svg'
+import thumbnailShoe1 from './thumbnail-shoe1.svg'
+import thumbnailShoe2 from './thumbnail-shoe2.svg'
+import thumbnailShoe3 from './thumbnail-shoe3.svg'
+
+export {
+    bigShoe1,
+    bigShoe2,
+    bigShoe3,
+
+    customer3,
+    customer4,
+
+    footerLogo,
+    montagneLogo,
+
+    offer,
+    shoe4,
+    shoe5,
+    shoe6,
+    shoe7,
+    shoe8,
+
+    thumbnailBackground,
+    thumbnailShoe1,
+    thumbnailShoe2,
+    thumbnailShoe3
+}
