f9de9f09f489e512609bf02982b048bd6a8fc9cc
Horacio Abitu <ahabitu@gmail.com>
2024-05-07 09:30:05 -0300

    upgrade image webp format

--- src/assets/images/index.js
+++ src/assets/images/index.js
@@ -1,6 +1,6 @@
 import bigShoe1 from './big-shoe1.webp'
-import bigShoe2 from './big-shoe2.png'
-import bigShoe3 from './big-shoe3.png'
+import bigShoe2 from './big-shoe2.webp'
+import bigShoe3 from './big-shoe3.webp'
 
 import customer3 from './customer3.webp'
 import customer4 from './customer4.webp'
@@ -10,16 +10,14 @@ import montagneLogo from './montagne-logo.svg'
 
 import offer from './offer.webp'
 
-import shoe4 from './shoe4.png'
-import shoe5 from './shoe5.png'
-import shoe6 from './shoe6.png'
-import shoe7 from './shoe7.png'
-import shoe8 from './shoe8.png'
+import shoe4 from './shoe4.webp'
+import shoe5 from './shoe5.webp'
+import shoe6 from './shoe6.webp'
+import shoe7 from './shoe7.webp'
+import shoe8 from './shoe8.webp'
 
 import thumbnailBackground from './thumbnail-background.svg'
-import thumbnailShoe1 from './thumbnail-shoe1.svg'
-import thumbnailShoe2 from './thumbnail-shoe2.svg'
-import thumbnailShoe3 from './thumbnail-shoe3.svg'
+
 
 export {
     bigShoe1,
@@ -39,8 +37,6 @@ export {
     shoe7,
     shoe8,
 
-    thumbnailBackground,
-    thumbnailShoe1,
-    thumbnailShoe2,
-    thumbnailShoe3
+    thumbnailBackground
+    
 }
