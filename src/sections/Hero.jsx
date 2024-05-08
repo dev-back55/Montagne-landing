5e50792dad431fd1214eb42e6df5474b6cd0622c
Horacio Abitu <ahabitu@gmail.com>
2024-05-08 06:09:56 -0300

    upgrade hero font responsive mobile first

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -13,12 +13,12 @@ const Hero = () => {
       id='home'
       className='w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container'
     >
-      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28'>
+      <div className='relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28'>
         <p className='text-xl font-montserrat text-montagne-light'>
           Our Winter collections
         </p>
 
-        <h1 className='mt-10 font-poppins text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold'>
+        <h1 className='mt-10 font-poppins text-6xl text-8xl max-sm:text-[62px] max-sm:leading-[72px] font-bold'>
           <span className='xl:bg-white xl:whitespace-nowrap relative z-10 pr-10'>
             The New Arrival
           </span>
